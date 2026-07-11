5bd8fee0c8f2f5a11db6e3cb166a79bf30494bfb
lgc35066-cmd <lgc35066@gmail.com>
2026-07-11 22:37:44 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+function welcome() {
+
+    document.getElementById("result").innerHTML =
+        "✅ مرحبًا بك في LGC Smart Farm OS Demo v0.1";
+
+}
+
+// تحديث تجريبي كل 5 ثوانٍ
+setInterval(function () {
+
+    console.log("LGC System Running...");
+
+}, 5000);
